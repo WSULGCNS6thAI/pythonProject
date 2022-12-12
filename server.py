793bcb1ee133d794034812317ebcab4f9b687bd3
519-bin <115973280+519-bin@users.noreply.github.com>
2022-12-12 10:27:19 +0900

    다중접속 서버

--- server.py
+++ server.py
@@ -1,41 +1,41 @@
-from socket import *
-import threading
-import time
+import socketserver
 
-# 메시지 전송 함수
-def send(sock):
-    while True:
-        sendData = input('>>>')
-        sock.send(sendData.encode('utf-8'))
+class MyHandler(socketserver.BaseRequestHandler):
+    users = {}
 
-# 메시지 수신 함수
-def receive(sock):
-    while True:
-        recvData = sock.recv(1024) # 1024 바이트 데이터 수신
-        print('상대방 :', recvData.decode('utf-8'))
+    def handle(self):
+        print(self.client_address)
+        while True:
+            self.request.send("채팅 닉네임을 입력하세요: ".encode())
+            nickname = self.request.recv(1024).decode()
+            if nickname in self.users:
+                self.request.send("이미 등록된 닉네임 입니다.\n".encode())
+            else:
+                self.users[nickname] = (self.request, self.client_address)
+                print(f"현재 {len(self.users)} 명 참여중..")
+                
+                for sock, _ in self.users.values():
+                    sock.send(f"{nickname} 님이 입장 했습니다.".encode())
+                break
+        while True:
+            msg = self.request.recv(1024)
+            if msg.decode() == "/bye":
+                print("exit client")
+                self.request.close()
+                break
+            for sock, _ in self.users.values():
+                sock.send(f"[{nickname}] {msg.decode()}".encode())
 
-port = 8081
+        if nickname in self.users:
+            del self.users[nickname]
+            for sock, _ in self.users.values():
+                sock.send(f"{nickname} 님이 퇴장 했습니다.".encode())
+            print("현재 {} 명 참여중".format(len(self.users)))
 
-# AF_INET : IPv4 사용
-# SOCK_STREAM : tcp 패킷 수신
-serverSock = socket(AF_INET, SOCK_STREAM)
-serverSock.bind(('', port))
-serverSock.listen(1) # n개 클라이언트 접속 허용
-
-print('%d번 포트로 접속 대기중...'%port)
-
-connectionSock, addr = serverSock.accept()
-
-print(str(addr), '에서 접속되었습니다.')
-
-# 쓰레드 생성
-sender = threading.Thread(target=send, args=(connectionSock,))
-receiver = threading.Thread(target=receive, args=(connectionSock,)) 
-
-sender.start()
-receiver.start()
-
-# 무한 루프
-while True: 
-    time.sleep(1) # 1초 대기 
+class ChatServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
+
+server = ChatServer(("", 8888), MyHandler)
+server.serve_forever()
+server.shutdown()
+server.server_close()
