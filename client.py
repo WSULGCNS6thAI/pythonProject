73c4c0ca7b8df03c40bb5fc157b696319249bba2
komool <67896210+komool@users.noreply.github.com>
2022-12-12 10:22:57 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,37 +1,22 @@
-from socket import *
-import threading
-import time
+import socket
+from threading import Thread
 
-# 메시지 전송 함수
-def send(sock):
+def recv_message(sock):
     while True:
-        sendData = input('>>>')
-        sock.send(sendData.encode('utf-8'))
+        msg = sock.recv(1024) # 1024 바이트 데이터 수신
+        print(msg.decode())
 
-# 메시지 수신 함수
-def receive(sock):
-    while True:
-        recvData = sock.recv(1024) # 1024 바이트 데이터 수신
-        print('상대방 :', recvData.decode('utf-8'))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 8274))
 
+th = Thread(target=recv_message, args=(sock,))
+th.daemon = True
+th.start()
 
-port = 8081
-
-# AF_INET : IPv4 사용
-# SOCK_STREAM : tcp 패킷 수신
-clientSock = socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1', port))
-
-print('접속 완료')
-
-# 쓰레드 생성
-sender = threading.Thread(target=send, args=(clientSock,))
-receiver = threading.Thread(target=receive, args=(clientSock,))
-
-sender.start()
-receiver.start()
-
-# 무한 루프
 while True:
-    time.sleep(1) # 1초 대기
-    pass
+    msg = input()
+    sock.send(msg.encode())
+    if msg == '/종료':
+        break
+
+sock.close()
